4cfb9d449e5767cffc775cf5b15852b7225ff7fd
Mambel30 <kelvinmambel30@gmail.com>
2024-09-17 00:49:10 -0500

    subir archivos base

---
+++ inmersion-dev-aula1-Proyecto-Base/main.js
@@ -0,0 +1,31 @@
+let cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+
+const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generar (){
+
+let numeroDigitado = parseInt (cantidad.value);
+
+    if(cantidad < 8){
+        alert("La cantidad de caracteres debe ser mayor a 8"); 
+
+    }
+for(let i=0; i< numeroDigitado; i++){
+
+let caracterAleatorio = cadenaCaracteres [math.floor (Math.random() * cadenaCaracteres.length);]
+console.log (caracterAleatorio);
+
+
+}
+
+}
+
+
+
+
+
+
+
+
+
